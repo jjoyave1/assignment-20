21c8869d155472c18abed5d6849f35ed573832e8
Joe Joyave <jjoyave1@gmail.com>
2015-06-17 09:17:23 -0400

    experimenting with color palettes

--- app/js/templates.js
+++ app/js/templates.js
@@ -4,9 +4,9 @@ this["hbs"]["desc"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"mai
 
   return "<div class=\"description\">\n\n  <span>Age: "
     + alias3(((helper = (helper = helpers.age || (depth0 != null ? depth0.age : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"age","hash":{},"data":data}) : helper)))
-    + "</span><br/>\n\n  <span>Favorite color:\n    <div class =\"colorWrap\">\n\n      <span class=\"colors\" style=\"background-color:"
+    + "</span><br/>\n\n  <div>Favorite color:</div>\n  <div class=\"colorwrap\"><div class=\"colors\" style=\"background-color:"
     + alias3(((helper = (helper = helpers.favColor || (depth0 != null ? depth0.favColor : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"favColor","hash":{},"data":data}) : helper)))
-    + "\"></span>\n\n    </div>\n\n  </span><br/>\n\n  <div class=\"imgwrap\" height=\"300px\" width=\"auto\">\n\n    <img src=\""
+    + "\"></div></div><br/>\n\n  <div class=\"imgwrap\" height=\"300px\" width=\"auto\">\n\n    <img src=\""
     + alias3(((helper = (helper = helpers.image || (depth0 != null ? depth0.image : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"image","hash":{},"data":data}) : helper)))
     + "\" class = \"pics\">\n\n  </div>\n\n  <button id=\"delete\" class=\"deleteContact\" data-id=\""
     + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
